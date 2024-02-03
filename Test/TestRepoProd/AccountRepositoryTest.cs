f9929946f84dcecb090bce83f878f075a05fe93f
ngocminh <ngocminh240402@gmail.com>
2024-02-03 11:44:29 +0700

    add Account Repo and Service Test

---
+++ Test/TestRepoProd/AccountRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Core.Repositories;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Test
+{
+    [TestClass]
+    public class AccountRepositoryTest
+    {
+        PMSDbContext _context;
+        IAccountRepository _repository;
+        IUnitOfWork _unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var loggerMock = new Mock<ILogger>();
+            _context = new PMSDbContext();
+            _repository = new AccountRepository(_context, loggerMock.Object);
+        }
+
+
+        [TestMethod]
+        public void Test3()
+        {
+            var result = _repository.GetAll();
+            Assert.IsNotNull(result.Result);
+        }
+    }
+}
---
+++ Test/TestServProd/AccountServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Core.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Test
+{
+    [TestClass]
+    public class AccountServiceTest
+    {
+        PMSDbContext _context;
+        AccountService _service;
+        IUnitOfWork _unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var loggerFactoryMock = new Mock<ILoggerFactory>();
+            _context = new PMSDbContext();
+
+            _unitOfWork = new UnitOfWork(_context, loggerFactoryMock.Object);
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+            });
+            var mapper = config.CreateMapper();
+            _service = new AccountService(_unitOfWork, mapper);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            var result = _service.GetAccountAsync().Result;
+            Assert.IsNotNull(result);
+        }
+    }
+}
